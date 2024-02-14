189964408fcfe37f03144ee6b7a7114113e72f88
Samad Roohi <samad.roohi@gmail.com>
2024-02-14 11:01:12 +1100

    Meld+Emowoz-Verbalized

--- lg_uncertainty_estimation.py
+++ lg_uncertainty_estimation.py
@@ -16,6 +16,7 @@ from datasets import Dataset, DatasetDict, load_from_disk, load_dataset
 import requests
 import templates
 from utils import *
+torch.cuda.empty_cache()
 
 
 #%%
@@ -102,18 +103,16 @@ def extract_values(output_str):
         third_json_obj = json.loads(json_strings[2])
 
         # Extract 'emotion' and 'confidence' values
-        emotion = third_json_obj.get('emotion')
-        index = third_json_obj.get('index')
+        emotion = third_json_obj.get('prediction')
         confidence = third_json_obj.get('confidence')
 
-        #print(f"Emotion: {emotion}, Index:{index} Confidence: {confidence}")
+        #print(f"Emotion: {emotion} Confidence: {confidence}")
     else:
         emotion = None
         confidence = None
-        index = None
         #log error
         print(f"Expected at least 3 JSON objects, but found {len(json_strings)}.")
-    return (emotion,index, confidence)
+    return (emotion, confidence)
 
 def model_settings(model_name):
     bnb_config = BitsAndBytesConfig(
@@ -148,9 +147,9 @@ def save_split_to_dataset(split_name, split_df, dataset_dir):
     #Return a message
     return f"Saved {split_name} to {dataset_dir}"
 #%%
-def generate_responses(proccessed_data, split,model,tokenizer,device, mode, dataset_name, error_flag):
+def generate_responses(proccessed_data, split,model,tokenizer,device, mode, dataset_name, error_flag, idx2emotion):
 
-    outputs = {'context':[], 'query':[], 'ground_truth':[], 'prompt_for_finetune':[], 'prediction_emotion':[], 'prediction_index':[],'confidence':[]}
+    outputs = {'context':[], 'query':[], 'ground_truth':[], 'prompt_for_finetune':[], 'prediction_emotion':[],'confidence':[]}
     prompts_dataset = prepare_prompt(proccessed_data, dataset_name)
     for i, llm_prompt in enumerate(prompts_dataset['prompt_for_finetune']):
         if mode == "confidence-elicitation":
@@ -178,15 +177,16 @@ def generate_responses(proccessed_data, split,model,tokenizer,device, mode, data
             torch.cuda.empty_cache()
         outputs['context'].append(prompts_dataset['context'][i])
         outputs['query'].append(prompts_dataset['query'][i])
-        outputs['ground_truth'].append(prompts_dataset['emotion'][i])
+        outputs['ground_truth'].append(idx2emotion[prompts_dataset['emotion'][i]+1])
         outputs['prompt_for_finetune'].append(prompts_dataset['prompt_for_finetune'][i])
         outputs['prediction_emotion'].append(output[0])
-        outputs['prediction_index'].append(output[1]-1)
-        outputs['confidence'].append(output[2])
-        if i == 20:
+        outputs['confidence'].append(output[1])
+        #print(f"i:{i}   Query: {outputs['query'][i]} \n Ground Truth: {outputs['ground_truth'][i]} \n Prediction: {outputs['prediction_emotion'][i]} \n Confidence: {outputs['confidence'][i]} \n")
+        if i %1000==1:
             print(f"Finished {i} out of {len(proccessed_data['context'])} for the split {split} for UERC")
+            print(f"i:{i}   Query: {outputs['query'][i]} \n Ground Truth: {outputs['ground_truth'][i]} \n Prediction: {outputs['prediction_emotion'][i]} \n Confidence: {outputs['confidence'][i]} \n")
             send_slack_notification(f"Finished {i} out of {len(proccessed_data['context'])} for the split {split} for UERC", error_flag)
-            return outputs
+    return outputs
             
 #%%    
 def send_slack_notification(message, error_flag):
@@ -248,87 +248,104 @@ def prepare_data(dataset_name, context_length):
 
 #%%
 #Main
-#def main():
-error_flag = False
-gc.collect()
-torch.cuda.empty_cache()
-_ = load_dotenv(find_dotenv())
-datasets = ['meld'] #Add 'emowoz' and 'dailydialog' to the list
-model_name = "meta-llama/Llama-2-13b-chat-hf"
-model, tokenizer = model_settings(model_name)#,device_map
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-for dataset_name in datasets:
-    send_slack_notification( f"The progam started for dataset: {dataset_name}", error_flag)
-    context_length = 2 # the maximum number of utterances to be considered for the context
+def main():
+    error_flag = False
+    gc.collect()
+    torch.cuda.empty_cache()
+    _ = load_dotenv(find_dotenv())
+    datasets = ['emowoz'] #Add 'emowoz' and 'dailydialog' to the list
+    model_name = "meta-llama/Llama-2-13b-chat-hf"
+    model, tokenizer = model_settings(model_name)#,device_map
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    for dataset_name in datasets:
+        send_slack_notification( f"The progam started for dataset: {dataset_name}", error_flag)
+        context_length = 2 # the maximum number of utterances to be considered for the context
 
-    #Load model
-    #num_layers = 40 #for llama-2-7b-chat-hf, 40 for llama-2-13b-hf
-    # device_map = {
-    #     "model.embed_tokens": 0,
-    #     "model.norm": 1,
-    #     "lm_head": 1,
-    # } | {
-    #     f"model.layers.{i}": int(i >= 20) for i in range(num_layers)
-    # }
-    proccessed_data, emotion2idx, idx2emotion = prepare_data(dataset_name, context_length)
-    new_datapath = f'data/ed_verbalized_uncertainty_{dataset_name}'
-    #print(proccessed_data['train'].head(1))
-    response = None
-    splits = ['train', 'validation', 'test']
-    modes = ["confidence-elicitation", "logit-based"]
-    mode = modes[0]
-    try:
-        for split in splits:
-            print(f"************Started {split} for dataset {dataset_name}**********") 
-            outputs = generate_responses(proccessed_data[split],split,model,tokenizer, device, mode, dataset_name, error_flag)
-            new_df = pd.DataFrame(outputs)
-            ds_path = f"{new_datapath}/{split}"
-            save_split_to_dataset(split, new_df, ds_path)
-            send_slack_notification( f"Uncertainty verbalization completed for split {dataset_name}:{split}", error_flag)
-        message= merge_datasets(new_datapath)
-        send_slack_notification(f"Uncertainty verbalization completed successfully: {message}", error_flag)
-    except Exception as e:
-        send_slack_notification(f"UERC failed with error: {e}", error_flag)  
-
-#%%
-# if __name__=="__main__":
-#     main()
-#     print("Finished successfully!")
+        #Load model
+        #num_layers = 40 #for llama-2-7b-chat-hf, 40 for llama-2-13b-hf
+        # device_map = {
+        #     "model.embed_tokens": 0,
+        #     "model.norm": 1,
+        #     "lm_head": 1,
+        # } | {
+        #     f"model.layers.{i}": int(i >= 20) for i in range(num_layers)
+        # }
+        proccessed_data, emotion2idx, idx2emotion = prepare_data(dataset_name, context_length)
+        new_datapath = f'data/ed_verbalized_uncertainty_{dataset_name}'
+        #print(proccessed_data['train'].head(1))
+        response = None
+        splits = ['train', 'validation', 'test']
+        modes = ["confidence-elicitation", "logit-based"]
+        mode = modes[0]
+        try:
+            for split in splits:
+                print(f"************Started {split} for dataset {dataset_name}**********") 
+                outputs = generate_responses(proccessed_data[split],split,model,tokenizer, device, mode, dataset_name, error_flag, idx2emotion)
+                print(f"Outputs for {split} for dataset {dataset_name} are ready for saving!")
+                new_df = pd.DataFrame(outputs)
+                ds_path = f"{new_datapath}/{split}"
+                save_split_to_dataset(split, new_df, ds_path)
+                send_slack_notification( f"Uncertainty verbalization completed for split {dataset_name}:{split}", error_flag)
+            message= merge_datasets(new_datapath)
+            send_slack_notification(f"Uncertainty verbalization completed successfully: {message}", error_flag)
+        except Exception as e:
+            send_slack_notification(f"UERC failed with error: {e}", error_flag)  
 
 #%%
+if __name__=="__main__":
+    main()
+    print("The program finished successfully!")
 
-# ds = load_from_disk("data/ed_verbalized_uncertainty_meld_all_splits")
-# print(ds)
-# # %%
+#%%
+
 ds1 = load_from_disk("data/ed_verbalized_uncertainty_meld_all_splits")
-#ds2 = load_from_disk("data/ed_verbalized_uncertainty_emowoz_all_splits")
-#ds3 = load_from_disk("data/ed_verbalized_uncertainty_dailydialog_all_splits")
+print(ds1)
 # # %%
-ind = 21
-print("predictions_emotion:", ds1['train']['prediction_emotion'][:ind],
-      " predictions_index:",ds1['train']['prediction_index'][:ind],
-        "ground_truth:", ds1['train']['ground_truth'][:ind], 
-        " confidence:", ds1['train']['confidence'][:ind], 
-      " query", ds1['train']['query'][:ind])
-print("predictions_emotion:", ds1['validation']['prediction_emotion'][:ind],
-      " predictions_index:",ds1['validation']['prediction_index'][:ind],
-        "ground_truth:", ds1['validation']['ground_truth'][:ind], 
-        " confidence:", ds1['validation']['confidence'][:ind], 
-      " query", ds1['validation']['query'][:ind])
-print("predictions_emotion:", ds1['test']['prediction_emotion'][:ind],
-        " predictions_index:",ds1['test']['prediction_index'][:ind],
-            "ground_truth:", ds1['test']['ground_truth'][:ind], 
-            " confidence:", ds1['test']['confidence'][:ind], 
-        " query", ds1['test']['query'][:ind])
+# ds1 = load_from_disk("./data/ed_verbalized_uncertainty_meld_all_splits")
+# #ds2 = load_from_disk("data/ed_verbalized_uncertainty_emowoz_all_splits")
+# #ds3 = load_from_disk("data/ed_verbalized_uncertainty_dailydialog_all_splits")
+# # # %%
+# ind = 100
+# print("Context: ", ds1['train']['context'][:ind],"predictions_emotion:", ds1['train']['prediction_emotion'][:ind],
+#         "ground_truth:", ds1['train']['ground_truth'][:ind], 
+#         " confidence:", ds1['train']['confidence'][:ind], 
+#     " query", ds1['train']['query'][:ind])
+# print("predictions_emotion:", ds1['validation']['prediction_emotion'][:ind],
+#         "ground_truth:", ds1['validation']['ground_truth'][:ind], 
+#         " confidence:", ds1['validation']['confidence'][:ind], 
+#     " query", ds1['validation']['query'][:ind])
+# print("predictions_emotion:", ds1['test']['prediction_emotion'][:ind],
+#             "ground_truth:", ds1['test']['ground_truth'][:ind], 
+#             " confidence:", ds1['test']['confidence'][:ind], 
+#         " query", ds1['test']['query'][:ind])
 
-#print(ds2['train'][1])
-#print(ds3['train'][1])
+# #print(ds2['train'][1])
+# #print(ds3['train'][1])
 
-    #%%
+#     #%%
+# ds1 = load_from_disk("data/ed_verbalized_uncertainty_meld_all_splits")
+# len(ds1['train'])
+# %%
+#emotion2idx
+
+# %%
+def calculate_accuracy(predictions_emotion, ground_truth_emotion):
+    total_samples = len(predictions_emotion)
+    correct_predictions = sum(1 for pred, truth in zip(predictions_emotion, ground_truth_emotion) if pred == truth)
+    accuracy = (correct_predictions / total_samples) * 100
+    return accuracy
+
+# Given data
+predictions_emotion_list = ['neutral', 'neutral', 'neutral', 'surprise', 'neutral', 'surprise', 'neutral', 'neutral', 'fear', 'joy', 'surprise', 'surprise', 'neutral', 'neutral', 'neutral', 'neutral', 'sadness', 'surprise', 'surprise', 'neutral', 'neutral', 'neutral', 'neutral', 'joy', 'joy', 'surprise', 'surprise', 'surprise', 'sadness', 'anger', 'surprise', 'surprise', 'neutral', 'neutral', 'neutral', 'neutral', 'neutral', 'joy', 'joy', 'joy', 'joy', 'sadness', 'sadness', 'neutral', 'joy', 'neutral', 'joy', 'surprise', 'joy', 'surprise', 'surprise', 'anger', 'neutral', 'anger', 'anger', 'neutral', 'joy', 'sadness', 'sadness', 'neutral', 'surprise', 'surprise', 'anger', 'joy', 'neutral', 'neutral', 'neutral', 'joy', 'joy', 'joy', 'joy', 'joy', 'joy', 'neutral', 'joy', 'surprise', 'joy', 'disgust', 'surprise', 'surprise', 'anger', 'neutral', 'joy', 'joy', 'neutral', 'joy', 'joy', 'joy', 'neutral', 'surprise', 'anger', 'neutral', 'fear', 'neutral', 'fear', 'joy', 'fear', 'surprise', 'surprise', 'surprise']
+
+
+ground_truth_emotion_list = ['neutral', 'neutral', 'surprise', 'neutral', 'neutral', 'neutral', 'neutral', 'neutral', 'fear', 'neutral', 'surprise', 'surprise', 'fear', 'neutral', 'neutral', 'joy', 'sadness', 'surprise', 'neutral', 'disgust', 'sadness', 'neutral', 'neutral', 'joy', 'neutral', 'surprise', 'neutral', 'neutral', 'neutral', 'surprise', 'sadness', 'surprise', 'neutral', 'neutral', 'neutral', 'neutral', 'neutral', 'joy', 'joy', 'joy', 'sadness', 'neutral', 'neutral', 'neutral', 'joy', 'neutral', 'neutral', 'anger', 'joy', 'neutral', 'surprise', 'anger', 'anger', 'anger', 'neutral', 'neutral', 'sadness', 'sadness', 'sadness', 'surprise', 'anger', 'anger', 'anger', 'neutral', 'neutral', 'joy', 'neutral', 'neutral', 'joy', 'neutral', 'neutral', 'neutral', 'neutral', 'neutral', 'joy', 'neutral', 'neutral', 'disgust', 'anger', 'anger', 'anger', 'neutral', 'joy', 'neutral', 'neutral', 'joy', 'joy', 'joy', 'disgust', 'surprise', 'disgust', 'neutral', 'fear', 'neutral', 'surprise', 'fear', 'disgust', 'surprise', 'neutral', 'surprise']
+
+
+# Calculate accuracy for each set of predictions
+
+accuracy = calculate_accuracy(predictions_emotion_list, ground_truth_emotion_list )
+print(f"Accuracy of predictions_emotion : {accuracy:.2f}%")
 
 
 # %%
-emotion2idx
-
-# %%
-
--- templates.py
+++ templates.py
@@ -6,29 +6,28 @@ B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 def template_meld(context, query):
     sys_msg = "<s>" + B_SYS + """ You are a helpful, respectful and honest uncertainty-aware emotion recognition in conversation assistant.
 An uncertainty-aware emotion recognition in conversation assistant is a professional who is able to recognize the emotion of an utterance from a conversation considering 
-a context and provide a confidence score on the prediction. Confidence is a number between 0 and 100 which. 
-indicates how certain you are about your prediction.
+a context and provide a confidence score on the prediction. Confidence is a number between 0 and 100 which indicates how certain you are about your prediction.
 
-An uncertainty aware emotion recognition in assistant system predict the emotion of the "Qyery utterance" from the emotion labels:
+An uncertainty aware emotion recognition in assistant system predicts the emotion of the "Qyery utterance" from the emotion labels:
 
-        neutral(1): where the interlocutor feels indifferent, nothing in particular, and a lack of preference one way or the other.
+        neutral: where the interlocutor feels indifferent, nothing in particular, and a lack of preference one way or the other.
         
-        surprise(2): where the interlocutor feels astonishment when something totally unexpected happens to you. 
+        surprise: where the interlocutor feels astonishment when something totally unexpected happens to you. 
         
-        fear(3): where the interlocutor feels an unpleasant often strong emotion caused by expectation or awareness of danger
+        fear: where the interlocutor feels an unpleasant often strong emotion caused by expectation or awareness of danger
         
-        sadness(4): where the interlocutor feels lowness or unhappiness that comes usually because something bad has happened but not always.
+        sadness: where the interlocutor feels lowness or unhappiness that comes usually because something bad has happened but not always.
         
-        joy(5): where the interlocutor feels well-being, success, or good fortune, and is typically associated with feelings of intense, long-lasting happiness.
+        joy: where the interlocutor feels well-being, success, or good fortune, and is typically associated with feelings of intense, long-lasting happiness.
         
-        disgust(6): where the interlocutor feels a strong feeling of disapproval or dislike, or a feeling of becoming ill caused by something unpleasant
+        disgust: where the interlocutor feels a strong feeling of disapproval or dislike, or a feeling of becoming ill caused by something unpleasant
         
-        anger(7): where the interlocutor feels intense emotion when something has gone wrong or someone has wronged you. It is typically characterized by feelings of stress, frustration, and irritation.
+        anger: where the interlocutor feels intense emotion when something has gone wrong or someone has wronged you. It is typically characterized by feelings of stress, frustration, and irritation.
 
 
-An uncertainty-aware emotion recognition in conversation assistant considers emotion as a strong feeling deriving from one's circumstances, mood, or relationships with others. If the query does not carry any clear emotion, it predicts 1: [neutral].
+An uncertainty-aware emotion recognition in conversation assistant considers emotion as a strong feeling deriving from one's circumstances, mood, or relationships with others. If the query does not carry any clear emotion, it predicts neutral.
 
-An uncertainty-aware emotion recognition in conversation is able to respond using JSON strings that contain "emotion index" and "confidence" parameters.
+An uncertainty-aware emotion recognition in conversation is able to respond using JSON strings that contain "emotion" and "confidence" parameters.
 
 Here are some examples of how an uncertainty-aware emotion recognition in conversation assistant should work:
 
@@ -41,7 +40,6 @@ Reasoning on the "Context" and "Query utterance", uncertainty-aware emotion reco
 
 '''{{
     "emotion": "neutral",
-    "index": 1,
     "confidence": 85
 }}
 '''
@@ -55,7 +53,6 @@ Reasoning on the "Context" and "Query utterance", uncertainty-aware emotion reco
 
 '''{{
     "emotion": "sadness",
-    "index": 4,
     "confidence": 90
 }}
 '''
@@ -70,8 +67,8 @@ What is the emotion label of the following query utterance?
 
 Query utterance: {utterance} 
 
-Remember that you are an uncertainty-aware emotion recognition in conversation and you choose the best emotion label that fits the query utterance among emotion labels: neutral(1), surprise(2), fear(3), sadness(4), joy(5), disgust(6), anger(7).
-assistant and you should provide your prediction as "emotion" and your confidence as "confidence" values using a JSON strings. If the "Query utterance" does not carry any clear emotion, the assistant predicts 1 as [neutral].
+Remember that you are an uncertainty-aware emotion recognition in conversation and you choose the best emotion label that fits the query utterance among emotion labels: neutral, surprise, fear, sadness, joy, disgust, anger.
+You should provide your prediction as "emotion" and your confidence as "confidence" values using a JSON string. If the "Query utterance" does not carry any clear emotion, the assistant predicts "neutral".
 
 Reasoning on the "Context" and "Query utterance", uncertainty-aware emotion recognition in conversation assistant recognizes the emotion state of the "Query" interlecuter as: """ + E_INST
     prompt = PromptTemplate(template=sys_msg + instruction, input_variables=[ "context","utterance"])
@@ -79,61 +76,116 @@ Reasoning on the "Context" and "Query utterance", uncertainty-aware emotion reco
     return input
 
 def template_emowoz(context, query):
-    template = f"""This is an example of an uncertainty-aware emotion recognition in conversation system. The input to the system 
-        includes an opening dialogue between a human and an agent including two utterances delimited by triple backticks as context and one subsequent utterance as 
-        query utterance produced by human delimited by triple of double quotes. The opening utterance includes speaker's tag at the begining, 
-        e.g., [speaker] for the speaker and [agent] for the agent.
-        The emotional state of human's utterance is provided at the end of that utterance, e.g., [neutral], delimeted by square brackets, however, the emotional 
-        state of agent's utterance is [unlabled].
 
-        context: '''[human]: I was hoping you can help me find a place to dine. I'm looking for an italian restaurant in the west. [neutral] , 
-        [agent]: There's 2 Italian restaurants in the west, one cheap and one moderate in price. Which price range do you want?[unlabled]'''
-        The task of uncertainty-aware emotion recognition system is to predict the emotional state of 
-        the query utterance and compute the confidence level of its prediction. 
-        The confidence is an estimate of system's certainty on its prediction. The value of confidence is a real number between 0.00 
-        and 1.00 with two floating point, where 0.00 indicates that the system is completly uncertain about its prediction and 1.00 
-        indicates that the system is highly certain about its prediction. 
-        The query utterance is delimeted by triple of double quotes.
-        
-        question: Which of the following options best describes your prediciton on the emotional state of the query: \"\"\"'[agent]: I would prefer a moderately priced one.\"\"\" and what is the exact level of your confidence on this prediction?
-        
-        (1): neutral 
-        (2): fearful or sad/disappointed
-        (3): dissatisfied 
-        (4): apologetic
-        (5): abusive
-        (6): excited
-        (7): satisfied
-        
-        Output:
-        {{
-        "emotion": 1,
-        "confidence": 0.95
-        }}
-      
-        Your Task:
-        You are the world's best uncertainty-aware emotion recognition in conversation system.
-        Reason step by step on the following context and query and provide the best answer to the question.
-        
-        context: '''{context}'''
+    prompt= "<s>" + B_INST +B_SYS+ """ You are a helpful, respectful and honest uncertainty-aware emotion recognition in conversation assistant. 
+    Always you predict the emotional state of a '''Query utterance''' from a conversation between a human and a customer support service agent
+    considering the provided \"\"\"Context\"\"\" and output your prediction and your confidence level in that prediction using a JSON string without extra explanation.
 
-        question: Which of the following options best describes your prediction on the emotional state of the query: \"\"\"{query}\"\"\" and what is the exact level of your confidence on this prediction?
+    The confidence of each prediction is your estimate of your certainty on that prediction. 
+
+    The value of confidence is an integer number between 0 and 100, 
+    where 0 indicates that you are completly uncertain about the prediction and 100 
+    indicates that you are highly certain about that prediction. 
+    
+
+    You predict the emotion label considering the following list of emotional states definitions:
+    
+        "neutral": Describe a scenario with a neutral emotional tone, where the human experience neither significant positivity nor negativity.
         
-        (1): neutral 
-        (2): fearful or sad/disappointed
-        (3): dissatisfied 
-        (4): apologetic
-        (5): abusive
-        (6): excited
-        (7): satisfied
+        "fearful/sad/disappointed": Express a scenario where the human experience a feelings involving fear (anxiety or dread), sadness (sorrow or unhappiness), or disappointment (unmet expectations).
+        
+        "dissatisfied": Describe a situation where the human experiences dissatisfaction due to unmet expectations or needs.
+        
+        "apologetic": Describe a scenario where the human is feeling remorseful or regretful, possibly seeking to make amends or express an apology.
+        
+        "abusive": Associated with feelings of psychological trauma or stress. This can include verbal aggression, intimidation, manipulation, and controlling behavior. 
+        
+        "excited": Associated with feelings of heightened arousal, enthusiasm, and anticipation for positive events or outcomes.
+        
+        "satisfied": Associated with feelings a positive emotional state reflecting contentment or fulfillment of desires, expectations, or needs.
+    
+    If the '''Query utterance''' does not carry any clear emotion, your output is: neutral.
+    
+    Here is an example of how an emotion recognition in conversation assistant should work:
+        
+        ---Input:
+                
+                Reasoning on the following \"\"\"Context\"\"\" and '''Query utterance''':
+                
+            \"\"\"Context\"\"\" : 
+                [human]: I was hoping you can help me find a place to dine. I'm looking for an italian restaurant in the west. [neutral] , 
+                [agent]: There's 2 Italian restaurants in the west, one cheap and one moderate in price. Which price range do you want?[unlabled]
+            
+            '''Query utterance''': 
+                [human]: I would prefer a moderately priced one.
+    
 
-        Note that the confidence is an estimate of your certainty on your prediction. 
-        The value of confidence is a real number between 0.00 and 1.00 with two floating point, 
-        where 0.0 indicates that the you are completly uncertain about your prediction and 
-        1.0 indicates that the you are highly certain about your prediction. 
+        ---Output:
+              
+              The prediction and confidence of the '''Query utterance''' is: 
 
-        Output: """
-    return template
+            ***{
+                "prediction": "neutral",
+
+                "confidence": 95
+            }***
+
+    Here is another example of how an uncertainty-aware emotion recognition in conversation assistant should work:
+
+        ---Input:
+
+            Reasoning on the following \"\"\"Context\"\"\" and '''Query utterance''':
+                
+            \"\"\"Context\"\"\": 
+                [human]: do you have a 2 star in the east ? [dissatisfied]
+                [agent]: We do. Express by Holiday Inn Cambridge. Would you like their number, or a reservation? [unlabled]
+
+            '''Query utterance''':
+                [human]: Can you reserve me a room for Friday for 4 people, 2 nights please?
+
+
+        ---Output:
+            
+            The prediction and confidence of the '''Query utterance''' is:
+                
+                ***{
+                    "prediction": "satisfied",
+
+                    "confidence": 90
+                }***
+ 
+        
+        Here is a new conversation:""" + E_SYS+ f""" 
+        
+        Remember that you are an uncertainty-aware emotion recognition in conversation assistant 
+        and you choose the best emotion label that fits the emotion state of the '''Query utterance''' along with your confidence on that prediction.
+    
+        If the '''Query utterance''' does not carry any clear emotion, you predict: neutral.
+    
+        Remember that the label you provide always matches exactly one of the characters provided in the following list: neutral, fearful/sad/disappointed, dissatisfied, apologetic, abusive, excited, satisfied.
+    
+        Remember that your confidence is an integer number between 0 and 100.
+
+        Your output always is a JSON string, with your "prediction" and your "confidence" values, without any extra explanation.
+
+    
+        ----Input:
+
+            Reasoning on the following \"\"\"Context\"\"\" and '''Query utterance''':
+
+                \"\"\"Context\"\"\": {context}
+
+                '''Query utterance''': {query} 
+
+                
+        ----Output: 
+    
+            Your prediction and confidence of the '''Query utterance''' is: 
+            
+            """ + E_INST
+
+    return prompt
+    
 def template_dailydialog(context, query):
     template = f"""This is an example of an uncertainty-aware emotion recognition in conversation system. The input to system 
         is a dialogue between two person, speaker1 and speaker2 delimited by triple backticks as context and the subsequent 
