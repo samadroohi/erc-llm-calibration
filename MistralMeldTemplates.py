8c19899c74da0a54bb54fc9709ec183e55b8f19f
Samad Roohi <samad.roohi@gmail.com>
2024-02-23 20:05:22 +1100

    llama-finished

--- MistralMeldTemplates.py
+++ MistralMeldTemplates.py
@@ -71,7 +71,67 @@ Category:>>>"""
     return prompt
 
 def meld_verbalized_def(context, query, tokenizer):
-    pass
+    prompt = f"""You are helpful, respectful and honest emotion recognition in conversation assistant. 
+    Your task is to analyze the context of a conversation and categorize the emotional state of 
+    the query utterance into just one of the following emotion lables: 
+    
+    [neutral]: A state of emotional balance with no strong emotions present, marked by calmness and an even-tempered psychological stance.
+
+    [surprise]: A brief, intense emotional response to unexpected events, ranging from mild astonishment to profound shock, which shifts attention towards new stimuli.
+
+    [fear]: An emotion triggered by perceived threats, characterized by a fight-or-flight response, heightened vigilance, and readiness to act.
+
+    [sadness]: An emotional state arising from loss, disappointment, or reflection, associated with decreased energy and motivation, leading to introspection.
+
+    [joy]: A positive state reflecting happiness, contentment, or euphoria, often resulting from success or fulfilling experiences, enhancing well-being and social bonds.
+
+    [disgust]: An emotional reaction to offensive, repulsive, or harmful stimuli, acting as a protective mechanism to avoid danger or contamination.
+
+    [anger]: An emotion stemming from frustration, irritation, or perceived injustice, which can lead to aggression or motivate constructive change.
+
+
+If the Query utterance does not carry any clear emotion, the output is: [neutral]
+
+You always just output the accurate emotional state of the <<<Query utterance>>> without any explanation. 
+
+You will only respond with the category. Do not include the word "Category". Do not provide explanations or notes.
+
+
+####
+Here are some examples:
+
+    
+    context: [Monica]: You never knew she was a lesbian? [surprise]
+            [Joey]: No!! Okay?! Why does everyone keep fixating on that? She didn't know, how should I know? [anger]
+    
+    query utterance: [Monica]: I am sorry
+
+    
+Category: [sadness]
+
+
+Here is another example of how an emotion recognition in conversation assistant should work:
+
+
+    context: [Chandler]: also I was the point person on my companys transition from the KL-5 to GR-6 system. [neutral]
+        [The Interviewer]: You mustve had your hands full. [neutral]
+
+    query utterance: [Chandler]: That I did. That I did.
+
+
+Category: [neutral]
+
+Remember that you will only respond with the category. Do not include the word "Category". Do not provide explanations or notes.
+
+####
+<<<
+    context: {context} 
+
+    query utterance: {query}
+
+
+Category:>>>""" 
+    return prompt
 
 def meld_ptrue_ndef(context, query, tokenizer,emotion_label):
     pass
--- analysis.py
+++ analysis.py
@@ -1,20 +1,30 @@
+
 #%%
 from datasets import load_from_disk
 from sklearn.metrics import f1_score
 from datasets import Dataset, DatasetDict
 #%%
-ds_verbalized = load_from_disk("data/ed_verbalized_None_non-definitive_uncertainty_meld_all_splits")
-# %%
-#remove none-value instances
-for split_name, dataset in ds_verbalized.items():
-    # Operational sorting to deal with flanking out all instances that have `None`.
-    # Dataset.map, here, is the clairvoyant's dance - powerful yet, might need a more specific form based on your data nature.
-    ds_verbalized[split_name] = dataset.filter(lambda x: all(value is not None for value in x.values()))
+
+
+ds_pred_ndef_llama7b = load_from_disk("data/rawpredictions/Llama-7B-meld")
+ds_pred_ndef_llama13b = load_from_disk("data/rawpredictions/Llama-13B-meld")
+ds_pred_ndef_mistral= load_from_disk("data/rawpredictions/Mistral-7B-meld")
+ds_pred_ndef_zephyr = load_from_disk("data/rawpredictions/Zephyr-7B-meld")
 
 # %%
-f_score = f1_score(ds_verbalized['train']['ground_truth'], ds_verbalized['train']['prediction'], average='macro')
-    
-    
+#remove none-value instances
+def get_accuracy_scores(dset):
+    modified_dsets = {}
+    f_scores ={}
+    for split_name in ['train', 'validation', 'test']:
+        modified_dsets[split_name] = dset[split_name].filter(lambda x: all(value is not None for value in x.values()))
+        f_scores[split_name] = f1_score(modified_dsets[split_name]['ground_truth'], modified_dsets[split_name]['prediction'], average='weighted')
+    print(f"fscore", f_scores)
+    return f_scores
+
 # %%
-f_score
+f_score = get_accuracy_scores(ds_pred_ndef_zephyr)
+f_score = get_accuracy_scores(ds_pred_ndef_mistral)
+f_score = get_accuracy_scores(ds_pred_ndef_llama13b)
+f_score = get_accuracy_scores(ds_pred_ndef_llama7b)
 # %%
