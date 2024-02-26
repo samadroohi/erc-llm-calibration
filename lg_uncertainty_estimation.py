bb5b8cfeab43be7db09db244cc0d93a6eccf237b
Samad <samad.roohi@gmail.com>
2024-02-26 11:42:03 +1100

    llama-zephyr-first

--- lg_uncertainty_estimation.py
+++ lg_uncertainty_estimation.py
@@ -226,6 +226,8 @@ def extract_lable_confidence(output_str):
         confidence = None
         #log error
         print(f"Expected exactly 1 JSON objects, but found {len(json_strings)}.")
+        print(f"Output string: {output_str}")
+
     return (emotion, confidence)  
 
 #%%
@@ -269,10 +271,10 @@ def generate_responses(proccessed_data, split,model,tokenizer,device, mode,  dat
             else:
                 outputs['prediction'].append(output)
                 outputs['confidence'].append(None)
-            #if i %100 == 1:
+            if i %100 == 1:
             #print(f"Finished {i} out of {len(proccessed_data['context'])} for the split {split} for UERC ")
             #send_slack_notification(f"Finished {i} out of {len(proccessed_data['context'])} for the split {split} for UERC", error_flag)
-            print( "Query: " , outputs['query'][i], ",      ground truth: ", outputs['ground_truth'][i], ",     prediction: ", 
+                print( "Query: " , outputs['query'][i], ",      ground truth: ", outputs['ground_truth'][i], ",     prediction: ", 
                 outputs['prediction'][i], "   , confidence:",  outputs['confidence'][i])
             torch.cuda.empty_cache()
 
