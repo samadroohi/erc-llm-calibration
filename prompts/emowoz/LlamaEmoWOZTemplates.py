26e8cfb1ccb9151abe99681334d77a191c679d0b
Samad Roohi <samad.roohi@gmail.com>
2024-03-06 19:27:48 +1100

    llamaemowoz

--- prompts/emowoz/LlamaEmoWOZTemplates.py
+++ prompts/emowoz/LlamaEmoWOZTemplates.py
@@ -4,6 +4,8 @@ B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 def template_emowoz(context, query, mode,tokenizer=None,emotion_label = None, stage_of_verbalization = None):
     if mode == "P(True)":
         prompt = emowoz_ptrue(context, query,tokenizer, emotion_label )
+    elif mode == "logit-based":
+        prompt = emowoz_logit(context, query,tokenizer, emotion_label )
     elif mode == 'verbalized':
         prompt = emowoz_verbalized(context, query,tokenizer,  stage_of_verbalization = stage_of_verbalization)
     return prompt
@@ -162,7 +164,76 @@ Remember that your confidence is an integer number between 0 and 100, indicatig 
 
 
 
+def emowoz_logit(context, query, tokenizer,emotion_label):
+    def meld_logit(context, query,tokenizer,emotion_label):
+     prompt= "<s>" + B_INST +B_SYS+ """ You are helpful, respectful and honest uncertainty-aware emotion recognition in conversation assistant. 
+You have two following tasks:
+     
+First, you always analyze the context and query utterances of a conversation and predict the emotional state of 
+    the query utterance into just one of the following emotion lables: 
+    
+        neutral: A state of being emotionally balanced, where an individual is not displaying a significant positive or negative emotional reaction. This state is often used as a baseline in emotional analysis.
 
+        disappointed: A feeling of sadness or displeasure caused by the non-fulfillment of one's hopes or expectations.
+
+        dissatisfied: A state of discontentment or unhappiness with an outcome, often when expectations are not met.
+
+        apologetic: A state expressing or showing regret or remorse for an action, typically for something that has caused inconvenience or harm to another.
+
+        abusive: An emotional state characterized by actions or words intended to harm or intimidate others. This can include verbal aggression, insults, or threats.
+
+        excited: A state of heightened emotional arousal, enthusiasm, or eagerness about something.
+
+        satisfie: A feeling of fulfillment or contentment with the outcomes or experiences, indicating that one's desires, expectations, or needs have been met.
+
+
+
+If the query utterance does not carry any clear emotion, the output is: neutral
+
+Second, you always provide your confidence on your prediction as an integer number between 0 and 100, where 0 indicates that you are completly uncertain about your prediction and 100 indicates that you are highly certain about that prediction. 
+
+You always provide the output in a JSON format, with your "prediction" and your "confidence" on that prediction, without any extra explanation.
+
+####
+
+Here is an example of how an emotion recognition in conversation assistant should work:        
+
+
+    Here is an examples:
+    
+    context :   [human]: I was hoping you can help me find a place to dine. I'm looking for an italian restaurant in the west. [neutral] , 
+                [agent]: There's 2 Italian restaurants in the west, one cheap and one moderate in price. Which price range do you want?[unlabled]
+            
+    query utterance: 
+        [human]: I would prefer a moderately priced one.
+
+    
+Output string: neutral
+
+
+Here is another example of how an emotion recognition in conversation assistant should work:
+
+
+    context:[human]: do you have a 2 star in the east ? [dissatisfied]
+            [agent]: We do. Express by Holiday Inn Cambridge. Would you like their number, or a reservation? [unlabled]
+
+    query utterance:
+        [human]: Can you reserve me a room for Friday for 4 people, 2 nights please?
+
+
+Output string: satisfied
+
+####""" + E_SYS+ f"""Remember that you always respond with just the most accurate emotion
+        label from the list of emotion lables: neutral, disappointed, dissatisfied, apologetic, abusive, excited, satisfie, without any explanations or notes. 
+        
+    what is your prediction for the following query utterance?
+ 
+    context: {context} 
+
+    query utterance: {query}
+
+""" + E_INST+ "Output string:" 
+     return prompt
 
 def emowoz_ptrue(context, query, tokenizer,emotion_label):
 
